8de9fe40ceec572ce1ff1c8108c0e365f08a79ef
kimbro97 <rlagudwog@gmail.com>
2025-08-27 17:23:50 +0900

    feat: 상품조회 예외처리 작업

--- src/main/java/com/server/ecommerce/support/exception/ValidationError.java
+++ src/main/java/com/server/ecommerce/support/exception/ValidationError.java
@@ -9,6 +9,11 @@ import lombok.Getter;
 @Getter
 public enum ValidationError {
 
+	SEARCH_PRODUCT_PAGE_INVALID(BAD_REQUEST, "page는 0 이상이어야 합니다"),
+	SEARCH_PRODUCT_SIZE_INVALID(BAD_REQUEST, "size는 2 이상이어야 합니다"),
+	SEARCH_PRODUCT_PRICE_INVALID(BAD_REQUEST, "minPrice와 maxPrice는 함께 제공되어야 합니다"),
+	SEARCH_PRODUCT_PRICE_RANGE_INVALID(BAD_REQUEST, "minPrice는 maxPrice보다 작아야 합니다"),
+
 	TEST_ERROR(BAD_REQUEST, "test error");
 
 	private final HttpStatus httpStatus;
