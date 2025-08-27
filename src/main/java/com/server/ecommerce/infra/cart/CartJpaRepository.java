6831887eb5c807f7d04224e0399b29e3f3a98ef3
kimbro97 <rlagudwog@gmail.com>
2025-08-27 20:24:47 +0900

    feat: CartRepository 구현체 작업

---
+++ src/main/java/com/server/ecommerce/infra/cart/CartJpaRepository.java
@@ -0,0 +1,13 @@
+package com.server.ecommerce.infra.cart;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.server.ecommerce.domain.cart.Cart;
+
+public interface CartJpaRepository extends JpaRepository<Cart, Long> {
+	Optional<Cart> findByProductId(Long productId);
+	List<Cart> findAllByUserId(Long userId);
+}
---
+++ src/main/java/com/server/ecommerce/infra/cart/CartRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.server.ecommerce.infra.cart;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.server.ecommerce.domain.cart.Cart;
+import com.server.ecommerce.domain.cart.CartRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class CartRepositoryImpl implements CartRepository {
+
+	private final CartJpaRepository cartJpaRepository;
+
+	@Override
+	public Cart save(Cart cart) {
+		return cartJpaRepository.save(cart);
+	}
+
+	@Override
+	public Optional<Cart> findById(Long cartId) {
+		return cartJpaRepository.findById(cartId);
+	}
+
+	@Override
+	public Optional<Cart> findByProductId(Long productId) {
+		return cartJpaRepository.findByProductId(productId);
+	}
+
+	@Override
+	public List<Cart> findAllByUserId(Long userId) {
+		return cartJpaRepository.findAllByUserId(userId);
+	}
+}
