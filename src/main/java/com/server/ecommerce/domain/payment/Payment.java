678e06e7cf43b4952d24cd26fca2f02746b96c8c
kimbro97 <rlagudwog@gmail.com>
2025-08-30 11:38:33 +0900

    feat: 결제 테이블 인덱스 설정

--- src/main/java/com/server/ecommerce/domain/payment/Payment.java
+++ src/main/java/com/server/ecommerce/domain/payment/Payment.java
@@ -7,12 +7,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(indexes = {
+	@Index(name = "idx_order_user_id", columnList = "user_id"),
+	@Index(name = "idx_order_order_id", columnList = "order_id"),
+	@Index(name = "idx_order_transaction_id", columnList = "transaction_id"),
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
 
