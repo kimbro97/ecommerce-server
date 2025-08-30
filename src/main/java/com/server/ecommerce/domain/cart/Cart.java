3a862d01918ff768cfb64b35b6b67ba7e2decc7e
kimbro97 <rlagudwog@gmail.com>
2025-08-30 11:56:58 +0900

    feat: 자바구니 테이블 인덱스 추가

--- src/main/java/com/server/ecommerce/domain/cart/Cart.java
+++ src/main/java/com/server/ecommerce/domain/cart/Cart.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +18,10 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(indexes = {
+	@Index(name = "idx_cart_user_id", columnList = "user_id"),
+	@Index(name = "idx_cart_product_id", columnList = "product_id"),
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart extends BaseEntity {
 
